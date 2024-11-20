4bacf7646be9dd3d7e8ca08d586b52c1d39fe7ed
Gustavo Tomsic <gusmat109@gmail.com>
2024-11-20 15:46:02 -0300

    camera.py module for camera support

--- src/robopi/__main__.py
+++ src/robopi/__main__.py
@@ -1,16 +1,6 @@
-import sys
-# For reaching system level dependencies when isolated in virtual environment
-# such as picamera2
-sys.path.append("/usr/lib/python3/dist-packages")
-
-from picamera2 import Picamera2, Preview
+from . import camera
 
 
-picam2 = Picamera2()
+im = camera.get_array()
 
-camera_config = picam2.create_preview_configuration()
-picam2.configure(camera_config)
-picam2.start_preview(Preview.NULL)
-
-picam2.start()
-im = picam2.capture_array() # returns numpy array
+print(im)
---
+++ src/robopi/camera.py
@@ -0,0 +1,34 @@
+"""
+robopi.camera
+~~~~~~~~~~~~~
+
+This module contains the camera support. Initially we are providing support
+for the Raspberry Pi Camera Rev 1.3. In the future this module should contain 
+also support for other cameras. This way we abstract the main program of the 
+mechanism by which the image array is provided
+"""
+
+import sys
+# For reaching system level dependencies when isolated in virtual environment
+# such as picamera2
+sys.path.append("/usr/lib/python3/dist-packages")
+
+import numpy as np
+from picamera2 import Picamera2, Preview
+
+
+picam2 = Picamera2()
+
+camera_config = picam2.create_preview_configuration()
+picam2.configure(camera_config)
+picam2.start_preview(Preview.NULL)
+
+picam2.start()
+
+def get_array() -> np.ndarray:
+    """
+    Function for supporting Raspberry Pi Camera Rev 1.3.
+
+    return: numpy.ndarray
+    """
+    return  picam2.capture_array() # returns numpy array
