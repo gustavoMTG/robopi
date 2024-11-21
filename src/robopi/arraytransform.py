95b3b4f1f11e7c868a4c6dde1d0c693cde341fc4
Gustavo Tomsic <gusmat109@gmail.com>
2024-11-21 15:12:03 +0000

    Corrected Red and Blue channels invertion and removed Alpha channel

--- src/robopi/arraytransform.py
+++ src/robopi/arraytransform.py
@@ -2,5 +2,10 @@ import numpy as np
 
 
 def arraytransform(frame: np.ndarray) -> np.ndarray:
-    print(frame.shape)
+    height, width, channels = frame.shape
+
+    frame[:, 20] = [255, 0, 0]
+    frame[:, -20] = [0, 255, 0]
+    frame[20, :] = [0, 0, 255]
+
     return frame
--- src/robopi/camera.py
+++ src/robopi/camera.py
@@ -19,7 +19,7 @@ from picamera2 import Picamera2, Preview
 
 picam2 = Picamera2()
 
-camera_config = picam2.create_preview_configuration()
+camera_config = picam2.create_preview_configuration({"format": "RGB888"})
 picam2.configure(camera_config)
 picam2.start_preview(Preview.NULL)
 
